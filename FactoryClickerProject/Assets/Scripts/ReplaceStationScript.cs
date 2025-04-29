80c13038b07f8f25c18bb5417eb03d2b630ad747
Ava-TU <C24384981@myTUDublin.ie>
2025-04-29 18:28:17 +0100

    Attempting Station disabling

---
+++ FactoryClickerProject/Assets/Scripts/ReplaceStationScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ReplaceStationScript : MonoBehaviour
+{
+    public UnityEvent changeStaion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
