7aa185c38b01a70a758e199af57de5d1464c1fb1
Ava-TU <C24384981@myTUDublin.ie>
2025-04-30 01:05:52 +0100

    added in all timer changes for disabling the buttons

--- FactoryClickerProject/Assets/Scripts/ReplaceStationScript.cs
+++ FactoryClickerProject/Assets/Scripts/ReplaceStationScript.cs
@@ -18,6 +18,9 @@ public class ReplaceStationScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //After the timer reaches the selected times shown, it'll invoke the chosen event in the if statement.
+
+        //In this case, when the time reaches 20 seconds, the button on the Design Station game object will switch off.
         if (timerScript.elapsedTime >= 20)
         {
             changeDesign.Invoke();
@@ -26,6 +29,21 @@ public class ReplaceStationScript : MonoBehaviour
         if (timerScript.elapsedTime >= 40)
         {
             changePrototype.Invoke();
+        }
+
+        if (timerScript.elapsedTime >= 60)
+        {
+            changeMolding.Invoke();
+        }
+
+        if (timerScript.elapsedTime >= 80)
+        {
+            changeProduction.Invoke();
+        }
+
+        if (timerScript.elapsedTime >= 100)
+        {
+            changeAssembly.Invoke();
         }
     }
 }
