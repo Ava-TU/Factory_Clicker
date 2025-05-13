f8ea95bcd98da61ec2402379afbbffc9a587bff5
Ava-TU <C24384981@myTUDublin.ie>
2025-05-14 00:43:15 +0100

    Made a screen sprite to display money

--- FactoryClickerProject/Assets/Scripts/ResourceTracker.cs
+++ FactoryClickerProject/Assets/Scripts/ResourceTracker.cs
@@ -60,8 +60,8 @@ public class ResourceTracker : MonoBehaviour
 
     private void UpdateUI()
     {
-        resourceCounter.text = "Money: " + resourcesAvailable.ToString();
-        clickCounter.text = "Money Per Second: " + (Mathf.Round(autoClicks * 10)/10).ToString();
+        resourceCounter.text = resourcesAvailable.ToString();
+        clickCounter.text = (Mathf.Round(autoClicks * 10)/10).ToString();
     }
 
 }
