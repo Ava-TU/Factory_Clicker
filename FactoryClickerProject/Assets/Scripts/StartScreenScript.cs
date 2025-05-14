c7d5090fe51ed36066542796c976ade1dfbd281e
Ava-TU <C24384981@myTUDublin.ie>
2025-05-14 14:23:54 +0100

    Made the start screen

---
+++ FactoryClickerProject/Assets/Scripts/StartScreenScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreenScript : MonoBehaviour
+{
+    
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        print("Quit Game");
+    }
+
+
+}
